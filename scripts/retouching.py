e429a802a08c5d7c0117be45f49720043634fa39
BK927 <dead4bees927@gmail.com>
2023-02-22 20:14:19 +0900

    add : batch function

--- scripts/retouching.py
+++ scripts/retouching.py
@@ -2,6 +2,7 @@ import os
 import sys
 import traceback
 import pandas as pd
+from torch import randint
 
 import modules.scripts as scripts
 import gradio as gr
@@ -153,6 +154,11 @@ class Script(scripts.Script):
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
 
+    def __add_denoising(self, float1, float2):
+        int1 = int(float1 * 100)
+        int2 = int(float2 * 100)
+        return float(int1 + int2) / 100
+
     def run(
         self,
         p,
@@ -170,8 +176,7 @@ class Script(scripts.Script):
     ):
 
         initial_info = None
-        initial_output_dir = output_dir
-
+        
         images = [
             file
             for file in [os.path.join(input_dir, x) for x in os.listdir(input_dir)]
@@ -184,10 +189,15 @@ class Script(scripts.Script):
         p.do_not_save_grid = True
         p.do_not_save_samples = True
 
+        batch_count = p.n_iter
+
+        p.batch_size = 1
+        p.n_iter = 1
+
         init_prompt = p.prompt
         img_len = len(images)
 
-        state.job_count = img_len * loops
+        state.job_count = img_len * loops * batch_count
         
         if is_traversing:
             state.job_count = state.job_count * int((traversing_end - traversing_start) / traversing_step)
@@ -204,184 +214,186 @@ class Script(scripts.Script):
             p.denoising_strength = traversing_start
             print(p.denoising_strength)
 
-        while True:
-            current_denoising = p.denoising_strength
-            # image iteration processs begins here
-            for idx, path in enumerate(images):
-                if state.interrupted:
-                    break
-                batch_images = []
-                print(f"Processing: {path}")
-                try:
-                    img = Image.open(path)
-                    batch_images.append((img, path))
+        for n in range(batch_count):
+            processing.fix_seed(p)
+            initial_output_dir = output_dir
+            output_dir = os.path.join(output_dir, str(p.seed))
+            base_output_dir = output_dir
 
-                except BaseException:
-                    print(f"Error processing {path}:", file=sys.stderr)
-                    print(traceback.format_exc(), file=sys.stderr)
+            while True:
+                current_denoising = p.denoising_strength
+                # image iteration processs begins here
+                for idx, path in enumerate(images):
+                    if state.interrupted:
+                        break
+                    batch_images = []
+                    print(f"Processing: {path}")
+                    try:
+                        img = Image.open(path)
+                        batch_images.append((img, path))
 
-                if len(batch_images) == 0:
-                    print("No images will be processed.")
-                    break
+                    except BaseException:
+                        print(f"Error processing {path}:", file=sys.stderr)
+                        print(traceback.format_exc(), file=sys.stderr)
 
-                state.job = f"{idx} out of {img_len}: {batch_images[0][1]}"
-                p.init_images = [x[0] for x in batch_images]
+                    if len(batch_images) == 0:
+                        print("No images will be processed.")
+                        break
 
-                if use_txt:
-                    img_name = os.path.splitext(os.path.basename(path))[0]
-                    print("img name: " + img_name)
-                    if img_name in txt_dict:
-                        with open(txt_dict[img_name], "r") as file:
-                            tags = file.readline()
-                            print("tags: " + tags)
-                            p.prompt = init_prompt + ", " + tags
+                    state.job = f"{idx} out of {img_len}: {batch_images[0][1]}"
+                    p.init_images = [x[0] for x in batch_images]
 
-                proc = None
-                all_images = []
+                    if use_txt:
+                        img_name = os.path.splitext(os.path.basename(path))[0]
+                        print("img name: " + img_name)
+                        if img_name in txt_dict:
+                            with open(txt_dict[img_name], "r") as file:
+                                tags = file.readline()
+                                print("tags: " + tags)
+                                p.prompt = init_prompt + ", " + tags
 
-                if is_rerun:
-                    processing.fix_seed(p)
-                    batch_count = p.n_iter
-                    p.extra_generation_params = {
-                        "Denoising strength change factor": denoising_strength_change_factor,
-                    }
-
-                    p.batch_size = 1
-                    p.n_iter = 1
-
-                    output_images, info = None, None
-                    initial_seed = None
-                    initial_info = None
-
-                    grids = []
-
-                    original_init_image = p.init_images
-                    # state.job_count = loops * batch_count
-
-                    initial_color_corrections = [
-                        processing.setup_color_correction(p.init_images[0])
-                    ]
-
-                    history = []
-
-                    for i in range(loops):
-                        p.n_iter = 1
-                        p.batch_size = 1
-                        p.do_not_save_grid = True
-
-                        if opts.img2img_color_correction:
-                            p.color_corrections = initial_color_corrections
-
-                        state.job = f"Iteration {i + 1}/{loops}"
-
-                        proc = process_images(p)
-
-                        if initial_seed is None:
-                            initial_seed = proc.seed
-                            initial_info = proc.info
-
-                        init_img = proc.images[0]
-
-                        p.init_images = [init_img]
-                        p.seed = proc.seed + 1
-                        p.denoising_strength = min(
-                            max(
-                                p.denoising_strength * denoising_strength_change_factor, 0.1
-                            ),
-                            1,
-                        )
-                        history.append(proc.images[0])
-
-                    proc.seed = initial_seed
-                    p.denoising_strength = current_denoising
-                    all_images += history
-                else:
-                    proc = process_images(p)
-
-                if initial_info is None:
-                    initial_info = proc.info
-
-                output_images = proc.images
-                if is_rerun:
-                    output_images = all_images
-
-                for output, (input_img, path) in zip(proc.images, batch_images):
-                    base_name = os.path.basename(path)
-
-                    # 이미지 저장 함수
-                    def save_image(img, filename, save_dir):
-                        comments = {}
-                        if len(model_hijack.comments) > 0:
-                            for comment in model_hijack.comments:
-                                comments[comment] = 1
-
-                        info = create_infotext(
-                            p, p.all_prompts, p.all_seeds, p.all_subseeds, comments, 0, 0
-                        )
-                        pnginfo = {}
-                        if info is not None:
-                            pnginfo["parameters"] = info
-
-                        params = ImageSaveParams(img, p, filename, pnginfo)
-                        before_image_saved_callback(params)
-                        fullfn_without_extension, extension = os.path.splitext(filename)
-
-                        info = params.pnginfo.get("parameters", None)
-
-                        def exif_bytes():
-                            return piexif.dump(
-                                {
-                                    "Exif": {
-                                        piexif.ExifIFD.UserComment: piexif.helper.UserComment.dump(
-                                            info or "", encoding="unicode"
-                                        )
-                                    },
-                                }
-                            )
-
-                        if extension.lower() == ".png":
-                            pnginfo_data = PngImagePlugin.PngInfo()
-                            for k, v in params.pnginfo.items():
-                                pnginfo_data.add_text(k, str(v))
-
-                            img.save(
-                                os.path.join(save_dir, filename), pnginfo=pnginfo_data
-                            )
-
-                        elif extension.lower() in (".jpg", ".jpeg", ".webp"):
-                            img.save(os.path.join(save_dir, filename))
-
-                            if opts.enable_pnginfo and info is not None:
-                                piexif.insert(
-                                    exif_bytes(), os.path.join(save_dir, filename)
-                                )
-                        else:
-                            img.save(os.path.join(save_dir, filename))
-                    
-                    # 실제 저장 하는 부분
-                    if is_traversing:
-                        output_dir = os.path.join(output_dir, str(round(p.denoising_strength, 2)))
+                    proc = None
+                    all_images = []
 
                     if is_rerun:
-                        for output_index, o in enumerate(all_images):
-                            temp = output_dir
-                            output_dir = os.path.join(
-                                output_dir, "loop" + str(output_index)
-                            )
-                            self.__create_folder(output_dir)
-                            save_image(o, base_name, output_dir)
-                            output_dir = temp
-                    else:
-                        save_image(output, base_name, output_dir)
+                        p.extra_generation_params = {
+                            "Denoising strength change factor": denoising_strength_change_factor,
+                        }
 
-                    output_dir = initial_output_dir
-            
-            if is_traversing is False:
-                break
-            else:
-                p.denoising_strength += traversing_step
-                if not p.denoising_strength <= traversing_end:
-                    p.denoising_strength = current_denoising
+
+                        output_images, info = None, None
+                        initial_seed = None
+                        initial_info = None
+
+                        initial_color_corrections = [
+                            processing.setup_color_correction(p.init_images[0])
+                        ]
+
+                        history = []
+
+                        for i in range(loops):
+                            p.n_iter = 1
+                            p.batch_size = 1
+                            p.do_not_save_grid = True
+
+                            if opts.img2img_color_correction:
+                                p.color_corrections = initial_color_corrections
+
+                            state.job = f"Iteration {i + 1}/{loops}"
+
+                            proc = process_images(p)
+
+                            if initial_seed is None:
+                                initial_seed = proc.seed
+                                initial_info = proc.info
+
+                            init_img = proc.images[0]
+
+                            p.init_images = [init_img]
+                            p.seed = proc.seed + 1
+                            p.denoising_strength = min(
+                                max(
+                                    p.denoising_strength * denoising_strength_change_factor, 0.1
+                                ),
+                                1,
+                            )
+                            history.append(proc.images[0])
+
+                        proc.seed = initial_seed
+                        p.denoising_strength = current_denoising
+                        all_images += history
+                    else:
+                        proc = process_images(p)
+
+                    if initial_info is None:
+                        initial_info = proc.info
+
+                    output_images = proc.images
+                    if is_rerun:
+                        output_images = all_images
+
+                    for output, (input_img, path) in zip(proc.images, batch_images):
+                        base_name = os.path.basename(path)
+
+                        # 이미지 저장 함수
+                        def save_image(img, filename, save_dir):
+                            comments = {}
+                            if len(model_hijack.comments) > 0:
+                                for comment in model_hijack.comments:
+                                    comments[comment] = 1
+
+                            info = create_infotext(
+                                p, p.all_prompts, p.all_seeds, p.all_subseeds, comments, 0, 0
+                            )
+                            pnginfo = {}
+                            if info is not None:
+                                pnginfo["parameters"] = info
+
+                            params = ImageSaveParams(img, p, filename, pnginfo)
+                            before_image_saved_callback(params)
+                            fullfn_without_extension, extension = os.path.splitext(filename)
+
+                            info = params.pnginfo.get("parameters", None)
+
+                            def exif_bytes():
+                                return piexif.dump(
+                                    {
+                                        "Exif": {
+                                            piexif.ExifIFD.UserComment: piexif.helper.UserComment.dump(
+                                                info or "", encoding="unicode"
+                                            )
+                                        },
+                                    }
+                                )
+
+                            if extension.lower() == ".png":
+                                pnginfo_data = PngImagePlugin.PngInfo()
+                                for k, v in params.pnginfo.items():
+                                    pnginfo_data.add_text(k, str(v))
+
+                                img.save(
+                                    os.path.join(save_dir, filename), pnginfo=pnginfo_data
+                                )
+
+                            elif extension.lower() in (".jpg", ".jpeg", ".webp"):
+                                img.save(os.path.join(save_dir, filename))
+
+                                if opts.enable_pnginfo and info is not None:
+                                    piexif.insert(
+                                        exif_bytes(), os.path.join(save_dir, filename)
+                                    )
+                            else:
+                                img.save(os.path.join(save_dir, filename))
+                        
+                        # 실제 저장 하는 부분
+                        if is_traversing:
+                            output_dir = os.path.join(output_dir, str(round(p.denoising_strength, 2)))
+
+                        if is_rerun:
+                            for output_index, o in enumerate(all_images):
+                                temp = output_dir
+                                output_dir = os.path.join(
+                                    output_dir, "loop" + str(output_index)
+                                )
+                                self.__create_folder(output_dir)
+                                save_image(o, base_name, output_dir)
+                                output_dir = temp
+                        else:
+                            self.__create_folder(output_dir)
+                            save_image(output, base_name, output_dir)
+
+                        output_dir = base_output_dir
+                
+                if is_traversing is False:
                     break
+                else:
+                    p.denoising_strength = self.__add_denoising(p.denoising_strength, traversing_step)
+                    if not p.denoising_strength <= traversing_end:
+                        p.denoising_strength = current_denoising
+                        break
+
+            output_dir = initial_output_dir
+            if is_traversing:
+                p.denoising_strength = traversing_start
 
         return Processed(p, [], p.seed, initial_info)
